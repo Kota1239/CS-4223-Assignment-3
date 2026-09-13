ed81e4a5fab453232e1aca136b7a1ffac94b413d
Kota1239 <dakotawlyons@gmail.com>
2026-09-12 23:45:58 -0500

    WebGL allow exceptions

--- WebGL/Build/WebGL.loader.js
+++ WebGL/Build/WebGL.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58139981,
+    wasmFileSize: 58652679,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
